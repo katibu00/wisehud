858bdec733972a7389407b809a58cc32d2b8317a
katibu01 <ukmisau@gmail.com>
2023-12-14 13:49:41 +0100

    update

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -21,7 +21,6 @@ class RegisterController extends Controller
 
     public function register(Request $request)
     {
-        return $request->all();
         DB::beginTransaction();
 
         try {
