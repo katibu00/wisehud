079aaba9fce1fbd2d7faf5bc5315fc328e8bfed2
katibu01 <ukmisau@gmail.com>
2023-12-16 15:05:39 +0100

    update

--- app/Http/Controllers/WalletControler.php
+++ app/Http/Controllers/WalletControler.php
@@ -1,25 +1,37 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use App\Models\ReservedAccount;
 use Illuminate\Http\Request;
+use App\Models\ReservedAccount;
 
 class WalletControler extends Controller
 {
     public function index()
     {
 
+        $query = ReservedAccount::where('customer_email', auth()->user()->email)->first();
 
-    $query = ReservedAccount::where('customer_email', auth()->user()->email)->first();
+        if ($query) {
+            $accounts = json_decode($query->accounts, true);
+        } else {
+            $accounts = [];
+        }
 
-    if ($query) {
-        $accounts = json_decode($query->accounts, true);
-    } else {
-        $accounts = [];
+        return view('wallet.index', compact('accounts'));
+
     }
 
-    return view('wallet.index', compact('accounts'));
+    public function getWalletBalance(Request $request)
+    {
+        // Get the authenticated user
+        $user = $request->user();
 
+        // Retrieve the wallet balance for the user
+        $walletBalance = $user->wallet->balance;
+
+        return response()->json([
+            'success' => true,
+            'wallet_balance' => $walletBalance,
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\WalletControler;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,3 +14,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/register', [RegisterController::class,'register']);
 Route::post('/login', [LoginController::class,'apiLogin']);
+
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/wallet/balance', [WalletControler::class, 'getWalletBalance']);
+});
